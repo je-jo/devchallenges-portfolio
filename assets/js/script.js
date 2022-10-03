1e3e5e9bcbeaef4c62fd91b4b0f60b1281031b72
Jelena Jovicic <jejovicic@gmail.com>
2022-10-03 19:44:47 +0200

    start pagination

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
-console.log("I'm a placeholder file");
+//pagination
+
+const paginationContainer = document.getElementById("container--pagination");
+const paginatedList = document.getElementById("paginated-list");
+const paginatedListItems = paginatedList.querySelectorAll("li");
+
+const paginationLimit = 3;
+const pageCount = Math.ceil(paginatedListItems.length / paginationLimit)
+let currentPage;
+
+function appendPageNumber(index) {
+    const pageNumber = document.createElement("button");
+    pageNumber.classList.add("btn", "btn--pagination");
+    pageNumber.textContent = index;
+    pageNumber.setAttribute("page-index", index)
+    pageNumber.setAttribute("aria-label", "Page " + index);
+    paginationContainer.appendChild(pageNumber)
+}
+
+const getNumbers = () => {
+    for (let i = 1; i <= pageCount; i++) {
+        appendPageNumber(i)
+    }
+}
+
+window.addEventListener("load", () => {
+    getNumbers();
+});
+
+

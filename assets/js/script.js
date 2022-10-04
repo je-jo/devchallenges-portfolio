351e41c7fde4b9d1009bc80245aa622d9c0fb42a
Jelena Jovicic <jejovicic@gmail.com>
2022-10-04 17:42:42 +0200

    pagination

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,53 @@
 //pagination
 
-const paginationContainer = document.getElementById("container--pagination");
-const paginatedList = document.getElementById("paginated-list");
-const paginatedListItems = paginatedList.querySelectorAll("li");
+const paginationButtonsContainer = document.getElementById("pagination-container");
+const buttons = [...paginationButtonsContainer.children];
+const paginatedListContainer = document.getElementById("paginated-list");
+const paginatedListContainerItems = paginatedListContainer.querySelectorAll("li");
 
-const paginationLimit = 3;
-const pageCount = Math.ceil(paginatedListItems.length / paginationLimit)
+const maxItemsPerPage = 3;
+const pageCount = Math.ceil(paginatedListContainerItems.length / maxItemsPerPage);
 let currentPage;
 
-function appendPageNumber(index) {
-    const pageNumber = document.createElement("button");
-    pageNumber.classList.add("btn", "btn--pagination");
-    pageNumber.textContent = index;
-    pageNumber.setAttribute("page-index", index)
-    pageNumber.setAttribute("aria-label", "Page " + index);
-    paginationContainer.appendChild(pageNumber)
+function createPaginationBtns(index) {
+    const paginationBtn = document.createElement("button");
+    paginationBtn.classList.add("btn", "btn--pagination");
+    paginationBtn.textContent = index;
+    paginationBtn.setAttribute("page-index", index)
+    paginationBtn.setAttribute("aria-label", "Page " + index);
+    paginationButtonsContainer.appendChild(paginationBtn)
 }
 
 const getNumbers = () => {
     for (let i = 1; i <= pageCount; i++) {
-        appendPageNumber(i)
+        createPaginationBtns(i);
     }
 }
 
+function setActivePage(pageNum) {
+    currentPage = pageNum;
+    const lastOnPreviousPage = (pageNum - 1) * maxItemsPerPage;
+    const lastOnActivePage = pageNum * maxItemsPerPage;
+    paginatedListContainerItems.forEach((item, index) => {
+        item.classList.add("hidden");
+        if (index >= lastOnPreviousPage && index < lastOnActivePage) {
+            item.classList.remove("hidden");
+        }
+    })
+}
+
 window.addEventListener("load", () => {
+    console.log("hello")
     getNumbers();
+    setActivePage(1);
+    const buttons = [...paginationButtonsContainer.children];
+    buttons.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            setActivePage(buttons.indexOf(btn) + 1)
+        })
+    })
 });
+
+
 
 

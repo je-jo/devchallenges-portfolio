416379a42b4552a71d65cf7b4d99be319ba7c03a
Jelena Jovicic <jejovicic@gmail.com>
2022-10-05 17:39:26 +0200

    working filtering and pagination

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const listContainer = document.getElementById("paginated-list");
-const listItems = [...listContainer.querySelectorAll("li")];
-let filteredItems = listItems;
+const allItems = [...listContainer.children];
+const listItems = [];
+
 
 //filtering 
 
@@ -8,41 +9,56 @@ const filterButtonsContainer = document.getElementById("filters-container");
 const filterButtons = [...filterButtonsContainer.children];
 
 filterButtons.forEach(btn => {
+    btn.classList.remove("active");
     btn.addEventListener("click", (e) => {
-        filteredItems = [];
-        listItems.forEach((item) => {
+        listItems.forEach(item => {
             item.classList.add("hidden");
+        });
+        listItems.length = 0;
+        allItems.forEach(item => {
             if (item.classList.contains(e.target.id)) {
-                item.classList.remove("hidden");
+                listItems.push(item)
             }
-        }); 
+        });
+        if (e.target.id === "all") {
+            listItems.push(...allItems);
+        }
+        //e.target.classList.add("active");
+        handlePaginationButtons();
+        setActivePage(1);
     });
 });
-
-
-
 
 //pagination
 
 const paginationButtonsContainer = document.getElementById("pagination-container");
 const pgnButtons = [];
 const maxItemsPerPage = 3;
-const pageCount = Math.ceil(listItems.length / maxItemsPerPage);
+const pageCount = () => Math.ceil(listItems.length / maxItemsPerPage);
 let currentPage = 1;
 
-function createButton(index) {
+function createButton(num) {
     const paginationBtn = document.createElement("button");
     paginationBtn.classList.add("btn", "btn--pagination");
-    paginationBtn.textContent = index;
-    paginationBtn.setAttribute("aria-label", "Page " + index);
+    paginationBtn.textContent = num;
+    paginationBtn.setAttribute("aria-label", "Page " + num);
     paginationButtonsContainer.appendChild(paginationBtn);
     pgnButtons.push(paginationBtn);
 }
 
-const createPaginationButtons = () => {
-    for (let i = 1; i <= pageCount; i++) {
+function handlePaginationButtons() {
+    pgnButtons.length = 0;
+    while (paginationButtonsContainer.firstChild) {
+        paginationButtonsContainer.firstChild.remove();
+    };
+    for (let i = 1; i <= pageCount(); i++) {
         createButton(i);
     }
+    pgnButtons.forEach(btn => {
+        btn.addEventListener("click", () => {
+            setActivePage(pgnButtons.indexOf(btn) + 1);
+        });
+    });
 }
 
 function setActivePage(num) {
@@ -63,19 +79,11 @@ function setActivePage(num) {
     })
 };
 
-
-
-
-
 window.addEventListener("load", () => {
+    listItems.push(...allItems);
     listItems.forEach((item) => {
         item.classList.add("hidden");
     });
-    createPaginationButtons();
+    handlePaginationButtons();
     setActivePage(1);
-    pgnButtons.forEach((btn) => {
-        btn.addEventListener("click", () => {
-            setActivePage(pgnButtons.indexOf(btn) + 1);
-        })
-    })
 });
